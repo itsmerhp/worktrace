4316ae2e59b142fd73e06d6ca04443e72909edc2
Rahul Patel <its.merhp@gmail.com>
2019-09-07 14:36:05 +0530

    Company Model

---
+++ protected/common/models/Company.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace common\models;
+
+class Company extends \common\models\base\CompanyBase
+{
+    
+}
---
+++ protected/common/models/base/CompanyBase.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace common\models\base;
+
+use Yii;
+
+/**
+ * This is the model class for table "company".
+*
+    * @property integer $id
+    * @property string $name
+    * @property string $email
+    * @property string $mobile
+    * @property string $address
+    * @property string $latitude
+    * @property string $longitude
+    * @property integer $status
+    * @property integer $created_at
+    * @property integer $updated_at
+*/
+class CompanyBase extends \yii\db\ActiveRecord
+{
+/**
+* @inheritdoc
+*/
+public static function tableName()
+{
+return 'company';
+}
+
+/**
+* @inheritdoc
+*/
+public function rules()
+{
+        return [
+            [['address'], 'string'],
+            [['status', 'created_at', 'updated_at'], 'integer'],
+            [['name', 'email', 'mobile', 'latitude', 'longitude'], 'string', 'max' => 255],
+        ];
+}
+
+/**
+* @inheritdoc
+*/
+public function attributeLabels()
+{
+return [
+    'id' => 'ID',
+    'name' => 'Name',
+    'email' => 'Email',
+    'mobile' => 'Mobile',
+    'address' => 'Address',
+    'latitude' => 'Latitude',
+    'longitude' => 'Longitude',
+    'status' => 'Status',
+    'created_at' => 'Created At',
+    'updated_at' => 'Updated At',
+];
+}
+}
