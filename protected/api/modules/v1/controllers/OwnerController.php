b5abccffee66f008d3afd4e5238ee5cc124035c8
Rahul Patel <its.merhp@gmail.com>
2019-09-07 14:33:30 +0530

    Company + Owner db structure

---
+++ protected/api/modules/v1/controllers/OwnerController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace api\modules\v1\controllers;
+
+/**
+ * Description of OwnerController
+ *
+ * @author kittu
+ */
+class OwnerController {
+    //put your code here
+}
